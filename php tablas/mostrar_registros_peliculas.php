cab2aebd49ac11dd036625d9d1e807967261ccb8
fas2019 <41647758+fas2019@users.noreply.github.com>
2019-07-08 20:55:18 -0300

    Update mostrar_registros_peliculas.php

--- php tablas/mostrar_registros_peliculas.php
+++ php tablas/mostrar_registros_peliculas.php
@@ -1,46 +1,93 @@
 <?php
 
-/////////////////////conexion SQL ////////////////////////////
+////////////////// CONEXION A LA BASE DE DATOS ////////////////////////////////////
 
 $host="sql8.main-hosting.eu.";
-$usuario="u644738137_alito";
-$contrasena="SZ17eSOr8Jn1Lv441";
+$modelo="u644738137_alito";
+$contraseña="SZ17eSOr8Jn1Lv441";
 $base="u644738137_fas2";
 
-$conexion= new mysqli($host,$usuario,$contrasena,$base);
+$conexion= new mysqli($host, $modelo, $contraseña, $base);
 if ($conexion -> connect_errno)
 {
-	die("fallo la conexion:(".$conexion -> mysqli_connect_errno().")".$conexion -> mysql_connect_error());
+	die("Fallo la conexion:(".$conexion -> mysqli_connect_errno().")".$conexion-> mysqli_connect_error());
 }
-///////////////////////consulta sql////////////////////////////////
+////////////////// VARIABLES DE CONSULTA////////////////////////////////////
 
-$orlando="SELECT * FROM Peliculas";
-$resorlando=$conexion->query($orlando);
-?>
+$where="";
+$nombre=$_POST['xnombre'];
+$tipo=$_POST['xtipo'];
 
 
+////////////////////// BOTON BUSCAR //////////////////////////////////////
 
-<html lang= "es">
+if (isset($_POST['buscar']))
+{
+
 	
-    <head>
-    	<title>Mostrar registros Orlando</title>
-        <meta charset = "utf-8">
-        <meta name="viewport" content="wigth=device-width, initial-scale=1, maximum-scale=1"/>
-        <link href="css/stilos.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-     </head>
-     
-     <body>
-     	<header>
-        	<div class="alert alert-info">
-            <h2>Mostrame los registros</h2>
-            </div>
-        </header>
-        
-        <section>
-        
-        	<table class="table">
-            	 <tr>
+
+	if (empty($_POST['xtipo']))
+	{
+		$where="where nombre like '".$nombre."%'";
+	}
+
+	else if (empty($_POST['xnombre']))
+	{
+		$where="where tipo='".$tipo."'";
+	}
+
+	else
+	{
+		$where="where nombre like '".$nombre."%' and tipo='".$tipo."'";
+	}
+}
+/////////////////////// CONSULTA A LA BASE DE DATOS ////////////////////////
+
+$Peliculas="SELECT * FROM Peliculas $where";
+$resPeliculas=$conexion->query($Peliculas);
+$restipo=$conexion->query($Peliculas);
+
+if(mysqli_num_rows($resPeliculas)==0)
+{
+	$mensaje="<h1>No hay registros que coincidan con su criterio de búsqueda.</h1>";
+}
+?>
+<html lang="es">
+
+	<head>
+		<title>Registro de Peliculas ya tenemos</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+		<link href="css/estilos.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+	</head>
+	<body>
+		<header>
+			<div class="alert alert-info">
+			<h2>Registro de Peliculas ya tenemos</h2>
+			</div>
+		</header>
+		<section>
+			<form method="post">
+				<input type="text" placeholder="Nombre..." name="xnombre"/>
+				<select name="xtipo">
+					<option value="">tipo </option>
+					<?
+						while ($registrotipo = $restipo->fetch_array(MYSQLI_BOTH))
+						{
+							echo '<option value="'.$registrotipo['tipo'].'">'.$registrotipo['tipo'].'</option>';
+						}
+					?>
+				</select>
+				
+
+				
+				<button name="buscar" type="submit">Buscar</button>
+			</form>
+			<table class="table">
+				<tr>
                  	<th>Pelicula</th>
                     <th>Actor</th>
                     <th>Director</th>
@@ -48,37 +95,29 @@ $resorlando=$conexion->query($orlando);
                     <th>Calidad</th>
                     <th>Ubicasion Item</th>
                     <th>Original o Copia</th>
-          
-  				</tr>
-                
-                <?php
-					while ($registroOrlando= $resorlando ->fetch_array(MYSQLI_BOTH))
-					
-					{
-						echo'<tr>
-								<td>'.$registroOrlando['nombre'].'</td>
-								<td>'.$registroOrlando['actor'].'</td>
-								<td>'.$registroOrlando['director'].'</td>
-								<td>'.$registroOrlando['estreno'].'</td>
-								<td>'.$registroOrlando['calidad'].'</td>
-								<td>'.$registroOrlando['ubicacion'].'</td>
-								<td>'.$registroOrlando['tipo'].'</td>
-						
-							 </tr>';	
-					}
-				
-				
+				</tr>
+
+				<?php
+
+				while ($registroPeliculas = $resPeliculas->fetch_array(MYSQLI_BOTH))
+				{
+
+					echo'<tr>
+								<td>'.$registroPeliculas['nombre'].'</td>
+								<td>'.$registroPeliculas['actor'].'</td>
+								<td>'.$registroPeliculas['director'].'</td>
+								<td>'.$registroPeliculas['estreno'].'</td>
+								<td>'.$registroPeliculas['calidad'].'</td>
+								<td>'.$registroPeliculas['ubicacion'].'</td>
+								<td>'.$registroPeliculas['tipo'].'</td>
+						 </tr>';
+				}
 				?>
-                
-                
-                
-            </table>	              
-        
-        
-        </section>
-        
-        <footer>
-        </footer>
-     </body>
- </html>
-               
+			</table>
+
+			<?
+				echo $mensaje;
+			?>
+		</section>
+	</body>
+</html>
