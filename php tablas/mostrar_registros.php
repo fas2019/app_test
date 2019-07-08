e241cd6fe3ad831ac450f6e257ebaf18aea40ac8
fas2019 <41647758+fas2019@users.noreply.github.com>
2019-07-08 20:54:51 -0300

    Update mostrar_registros.php

--- php tablas/mostrar_registros.php
+++ php tablas/mostrar_registros.php
@@ -1,86 +1,129 @@
 <?php
 
-/////////////////////conexion SQL ////////////////////////////
+////////////////// CONEXION A LA BASE DE DATOS ////////////////////////////////////
 
 $host="sql8.main-hosting.eu.";
 $usuario="u644738137_alito";
-$contrasena="SZ17eSOr8Jn1Lv441";
+$contraseña="SZ17eSOr8Jn1Lv441";
 $base="u644738137_fas2";
 
-$conexion= new mysqli($host,$usuario,$contrasena,$base);
+$conexion= new mysqli($host, $usuario, $contraseña, $base);
 if ($conexion -> connect_errno)
 {
-	die("fallo la conexion:(".$conexion -> mysqli_connect_errno().")".$conexion -> mysql_connect_error());
+	die("Fallo la conexion:(".$conexion -> mysqli_connect_errno().")".$conexion-> mysqli_connect_error());
 }
-///////////////////////consulta sql////////////////////////////////
+////////////////// VARIABLES DE CONSULTA////////////////////////////////////
 
-$orlando="SELECT * FROM Orlando";
-$resorlando=$conexion->query($orlando);
-?>
+$where="";
+$usuario=$_POST['xusuario'];
+$Destino=$_POST['xDestino'];
 
 
+////////////////////// BOTON BUSCAR //////////////////////////////////////
 
-<html lang= "es">
+if (isset($_POST['buscar']))
+{
+
 	
-    <head>
-    	<title>Mostrar registros Orlando</title>
-        <meta charset = "utf-8">
-        <meta name="viewport" content="wigth=device-width, initial-scale=1, maximum-scale=1"/>
-        <link href="css/stilos.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-     </head>
-     
-     <body>
-     	<header>
-        	<div class="alert alert-info">
-            <h2>Mostrame los registros</h2>
-            </div>
-        </header>
-        
-        <section>
-        
-        	<table class="table">
-            	 <tr>
-                 	<th>usuario</th>
-                    <th>Local</th>
-                    <th>Producto</th>
-                    <th>Cantidad</th>
-                    <th> U$S </th>
-                    <th>Tipo Cambio</th>
-                    <th>Fecha</th>
-                    <th>Precio</th>
-                    <th>Precio Unidad</th>
-  				</tr>
-                
-                <?php
-					while ($registroOrlando= $resorlando ->fetch_array(MYSQLI_BOTH))
-					
-					{
-						echo'<tr>
-								<td>'.$registroOrlando['usuario'].'</td>
-								<td>'.$registroOrlando['Local'].'</td>
-								<td>'.$registroOrlando['Producto'].'</td>
-								<td>'.$registroOrlando['Cantidad'].'</td>
-								<td>'.$registroOrlando['Importe_Dolar'].'</td>
-								<td>'.$registroOrlando['TC'].'</td>
-								<td>'.$registroOrlando['Fecha'].'</td>
-								<td>'.$registroOrlando['Pesos'].'</td>
-								<td>'.$registroOrlando['pesos_unidad'].'</td>
-							 </tr>';	
-					}
+
+	if (empty($_POST['xusuario']))
+	{
+		$where="where Destino like '".$Destino."%'";
+	}
+
+	else if (empty($_POST['xDestino']))
+	{
+		$where="where usuario='".$usuario."'";
+	}
+
+	else
+	{
+		$where="where usuario like '".$usuario."%' and Destino='".$Destino."'";
+	}
+}
+/////////////////////// CONSULTA A LA BASE DE DATOS ////////////////////////
+
+$Orlando="SELECT * FROM Orlando $where";
+$resOrlando=$conexion->query($Orlando);
+$resDestino=$conexion->query($Orlando);
+
+if(mysqli_num_rows($resOrlando)==0)
+{
+	$mensaje="<h1>No hay registros que coincidan con su criterio de búsqueda.</h1>";
+}
+?>
+<html lang="es">
+
+	<head>
+		<title>Registros de Viaje</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+		<link href="css/estilos.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+	</head>
+	<body>
+		<header>
+			<div class="alert alert-info">
+			<h2>Registros de Viaje</h2>
+			</div>
+		</header>
+		<section>
+			<form method="post">
+				<input type="text" placeholder="Nombre..." name="xusuario"/>
+				<select name="xDestino">
+					<option value="">Destino </option>
+					<?
+						while ($registroDestino = $resDestino->fetch_array(MYSQLI_BOTH))
+						{
+							echo '<option value="'.$registroDestino['Destino'].'">'.$registroDestino['Destino'].'</option>';
+						}
+					?>
+				</select>
 				
+
 				
+				<button name="buscar" type="submit">Buscar</button>
+			</form>
+			<table class="table">
+				<tr>
+					<th>Usuario</th>
+					<th>Local</th>
+					<th>Producto</th>
+					<th>Cantidad</th>
+					<th>Importe_Dolar</th>
+					<th>TC</th>
+					<th>Fecha</th>
+					<th>Pesos</th>
+					<th>pesos_unidad</th>
+					<th>Destino</th>
+				</tr>
+
+				<?php
+
+				while ($registroOrlando = $resOrlando->fetch_array(MYSQLI_BOTH))
+				{
+
+					echo'<tr>
+						 <td>'.$registroOrlando['usuario'].'</td>
+						 <td>'.$registroOrlando['Local'].'</td>
+						 <td>'.$registroOrlando['Producto'].'</td>
+						 <td>'.$registroOrlando['Cantidad'].'</td>
+						 <td>'.$registroOrlando['Importe_Dolar'].'</td>
+						 <td>'.$registroOrlando['TC'].'</td>
+						 <td>'.$registroOrlando['Fecha'].'</td>
+						 <td>'.$registroOrlando['Pesos'].'</td>
+						 <td>'.$registroOrlando['pesos_unidad'].'</td>
+						 <td>'.$registroOrlando['Destino'].'</td>
+						 </tr>';
+				}
 				?>
-                
-                
-                
-            </table>	              
-        
-        
-        </section>
-        
-        <footer>
-        </footer>
-     </body>
- </html>
-               
+			</table>
+
+			<?
+				echo $mensaje;
+			?>
+		</section>
+	</body>
+</html>
